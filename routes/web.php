375a06d83b6a21844917d2ed8eeba5fb13c0beef
Hatallika <essenadmin@gmail.com>
2022-01-03 12:00:04 +0300

    lesson 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello/{name}',
+    fn (string $name) => "Hello, {$name}");
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/news', function () {
+    return view('news');
+});
